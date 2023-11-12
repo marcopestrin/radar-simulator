52d91afc94070515a0314864c551570732ed1ca6
pestrinmarco <pestrinmarco@gmail.com>
2023-11-12 14:50:28 +0100

    percentage

--- src/redux/controllers/now.tsx
+++ src/redux/controllers/now.tsx
@@ -1,55 +1,91 @@
 import moment from 'moment-timezone'
-import { routes, Route } from '../../model/routes';
+import { routes } from '../../model/routes';
 import { airports, Airport } from '../../model/airports';
 import { aircrafts } from '../../model/aircrafts';
+import { InAirAircraft } from '../../interfaces/aircraft/inAirAircraft';
+import { ActiveRoute } from '../../interfaces/route/activeRoute';
 
 export const getCurrentSituation = () => {
-  const activeRoutes = getActiveRoutes();
-  let aircraftsInAir: { id: number; type: string; }[] = [];
-  if (activeRoutes.length) {
-    aircraftsInAir = activeRoutes.map(({ id: idRoute }: Route) => getAircraftInAir(idRoute)) || [];
-  }
-  return {
-    aircraftsInAir,
-    activeRoutes
-  }
-}
 
-
-const getTimezoneByAirportId = (id: number) => {
-  const airport: Airport = airports.find(airport => airport.id === id)!;
-  return airport.timezoneName;
-}
-
-const getActiveRoutes = () => {
-  const workingRoutes = routes.filter(route => {
-
+  const activeRoutes = routes.map(route => {
     const timezoneDeparture = getTimezoneByAirportId(route.airportStart);
     const timezoneArrive = getTimezoneByAirportId(route.airportEnd);
     
     const currentHourAirportDeparture = moment().tz(timezoneDeparture).hour();
     const currentHourAirportArrive = moment().tz(timezoneArrive).hour();
 
+    // let percentage = 0;
+    let missedHour = 0;
+    let hoursInFlight = 0;
+
     if (route.nextDay) {
-      if (currentHourAirportDeparture > route.timeStart && currentHourAirportArrive > route.timeEnd) {
-        return true;
+      if (currentHourAirportDeparture > route.timeStart && currentHourAirportArrive < route.timeEnd) {
+        
+        hoursInFlight = currentHourAirportDeparture - route.timeStart;
+
+        // console.log({ hoursInFlight })
+        // console.log("******",percentage, timezoneDeparture, timezoneArrive);
+        return {
+          id: route.id,
+          percentage: getPercentage(hoursInFlight, route.duration),
+        };
+      }
+      if (currentHourAirportArrive < route.timeEnd) {
+        missedHour = route.timeEnd - currentHourAirportArrive;
+        hoursInFlight = route.duration - missedHour;
+        // console.log("---",percentage, timezoneDeparture, timezoneArrive);
+        return {
+          id: route.id,
+          percentage: getPercentage(hoursInFlight, route.duration),
+        }
       }
     } else {
       if (currentHourAirportDeparture > route.timeStart && currentHourAirportArrive < route.timeEnd) {
-        return true;
+        const hoursInFlight = currentHourAirportDeparture - route.timeStart;
+
+        // console.log("####",percentage, timezoneDeparture, timezoneArrive);
+        return {
+          id: route.id,
+          percentage: getPercentage(hoursInFlight, route.duration),
+        };
       }
     }
 
-    return false;
-  });
-  return workingRoutes;
-  
+    return {
+      id: route.id,
+      percentage: 0,
+    };
+  }).filter(({ percentage}) => percentage !== 0);
+
+  let aircraftsInAir: InAirAircraft[] = []; // empty array
+  if (activeRoutes.length) {
+    aircraftsInAir = activeRoutes.map(({ id, percentage }) => {
+      return {
+        ...getAircraftInAir(id),
+        percentage,
+      }
+    }) || [];
+  }
+  return {
+    aircraftsInAir,
+    activeRoutes
+  }
+};
+
+
+const getTimezoneByAirportId = (id: number) => {
+  const airport: Airport = airports.find(airport => airport.id === id)!;
+  return airport.timezoneName;
+};
+
+const getPercentage = (hoursInFlight: number, duration: number) => {
+  return Math.floor(hoursInFlight / duration * 100);
 }
 
 const getAircraftInAir = (idRoute: number) => {
   const aircraft = aircrafts.find(aircraft => {
     return aircraft.routes.find(route => route === idRoute);
-  })!
+  })!;
   return {
     id: aircraft.id,
     type: aircraft.type
